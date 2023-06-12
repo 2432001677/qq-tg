f3af7d0a7d63a5edd77baa862862a763f7c69cef
bruce <2432001677@qq.com>
2023-06-13 01:49:20 +0800

    use mirai 2.14.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,14 @@
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.8.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.13.2"
+    id("net.mamoe.mirai-console") version "2.14.0"
 }
 
-group = "org.example"
+group = "org.github"
 version = "0.1.0"
 
 repositories {
-    if (System.getenv("CI")?.toBoolean() != true) {
-        maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
-    }
     mavenCentral()
 }
--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,4 +1,4 @@
-package org.example.mirai.plugin
+package org.github.qqTg
 
 import net.mamoe.mirai.console.permission.AbstractPermitteeId
 import net.mamoe.mirai.console.permission.PermissionService
@@ -17,32 +17,16 @@ import net.mamoe.mirai.message.data.Image.Key.queryUrl
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.utils.info
 
-/**
- * 使用 kotlin 版请把
- * `src/main/resources/META-INF.services/net.mamoe.mirai.console.plugin.jvm.JvmPlugin`
- * 文件内容改成 `org.example.mirai.plugin.PluginMain` 也就是当前主类全类名
- *
- * 使用 kotlin 可以把 java 源集删除不会对项目有影响
- *
- * 在 `settings.gradle.kts` 里改构建的插件名称、依赖库和插件版本
- *
- * 在该示例下的 [JvmPluginDescription] 修改插件名称，id和版本，etc
- *
- * 可以使用 `src/test/kotlin/RunMirai.kt` 在 ide 里直接调试，
- * 不用复制到 mirai-console-loader 或其他启动器中调试
- */
-
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
-        id = "org.example.mirai-example",
+        id = "org.github.qq-tg",
         name = "插件示例",
         version = "0.1.0"
     ) {
-        author("作者名称或联系方式")
+        author("bruce")
         info(
             """
-            这是一个测试插件, 
-            在这里描述插件的功能和用法等.
+                qq telegram互通插件.
         """.trimIndent()
         )
         // author 和 info 可以删除.
